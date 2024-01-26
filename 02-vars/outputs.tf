d9e5b163b3d4d8dffd43b89972c62096e8670a18
SHASHIKANT NAIK <shashikant.naik@microfocus.com>
2024-01-26 20:40:54 +0530

    creation of topic 1,2

--- 02-vars/outputs.tf
+++ 02-vars/outputs.tf
@@ -18,10 +18,14 @@ output "value_in_map" {
   value = var.list["abc"]
 }
 
-output "second_in_map" {
+output "second_in_map_lookup" {
   value = lookup(var.list,"abc1","none")
 }
 
 output "Fruits_details" {
   value = "Fruit name - apple, qty - ${var.fruits.apple.qty}"
+}
+
+output "Orange_price_lookup" {
+  value = "Orange Price: ${lookup(var.fruits,["orange"]{"price"],"none")}"
 }
