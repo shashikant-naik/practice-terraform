02c14896bd6df305b907e8ac4a8a0f893236eb0d
SHASHIKANT NAIK <shashikant.naik@microfocus.com>
2024-01-26 22:04:43 +0530

    creation of topic 1,2

---
+++ expense-v1/main.tf
@@ -0,0 +1,13 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_instance" "frontend" {
+  ami                     = "ami-0f3c7d07486cad139"
+  instance_type           = "t3.micro"
+  vpc_security_group_ids  = ["sg-020d530177af68806"]
+
+  tags = {
+    Name = "frontend-dev"
+  }
+}
--- expense-v1/variable.tf
+++
@@ -1,9 +0,0 @@
-resource "aws_instance" "frontend" {
-  ami           = "ami-0f3c7d07486cad139"
-  instance_type = "t3.micro"
-  vpc_security_group_ids = ["sg-020d530177af68806"]
-
-  tags = {
-    Name = "frontend-dev"
-  }
-}
