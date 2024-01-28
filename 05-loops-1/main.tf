725b8d151b4fcfbb4947212797b17fb79fa790b4
SHASHIKANT NAIK <shashikant.naik@microfocus.com>
2024-01-28 20:03:32 +0530

    exp-v2

--- 05-loops-1/main.tf
+++ 05-loops-1/main.tf
@@ -6,3 +6,14 @@ resource "null_resource" "test" {
 output "test" {
   value = null_resource.test.*.id
 }
+
+resource "aws_instance" "test" {
+  count                   = 2
+  ami                     = var.ami
+  instance_type           = var.instance_type
+  vpc_security_group_ids  = [var.vpc_security_group_ids]
+
+  tags = {
+    Name = "test-${count.index}"
+  }
+}
