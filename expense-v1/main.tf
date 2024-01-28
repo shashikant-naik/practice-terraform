97a3f825484aaac0ac557b12e7030bc9ae2b7bd0
SHASHIKANT NAIK <shashikant.naik@microfocus.com>
2024-01-28 09:59:08 +0530

    creation of topic 1,2

--- expense-v1/main.tf
+++ expense-v1/main.tf
@@ -20,39 +20,3 @@ resource "aws_route53_record" "frontend" {
   zone_id = "Z07947133KI5JSIYK8TJ6" #Copy from console
   records = [aws_instance.frontend.public_ip]
 }
-
-resource "aws_instance" "backend" {
-  ami                     = "ami-0f3c7d07486cad139"
-  instance_type           = "t3.micro"
-  vpc_security_group_ids  = ["sg-020d530177af68806"]
-
-  tags = {
-    Name = "backend-dev"
-  }
-}
-
-resource "aws_route53_record" "backend" {
-  name    = "frontend-dev"
-  type    = "A"
-  ttl     = 30
-  zone_id = "Z07947133KI5JSIYK8TJ6" #Copy from console
-  records = [aws_instance.backend.public_ip]
-}
-
-resource "aws_instance" "mysql" {
-  ami                     = "ami-0f3c7d07486cad139"
-  instance_type           = "t3.micro"
-  vpc_security_group_ids  = ["sg-020d530177af68806"]
-
-  tags = {
-    Name = "mysql-dev"
-  }
-}
-
-resource "aws_route53_record" "mysql" {
-  name    = "frontend-dev"
-  type    = "A"
-  ttl     = 30
-  zone_id = "Z07947133KI5JSIYK8TJ6" #Copy from console
-  records = [aws_instance.mysql.public_ip]
-}
