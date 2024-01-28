406b3d024fbf010d04f310a1437036135afe1a38
SHASHIKANT NAIK <shashikant.naik@microfocus.com>
2024-01-28 11:40:32 +0530

    exp-v2

---
+++ expense-v2/main.tf
@@ -0,0 +1,23 @@
+provider "aws" {
+  region = "us-east-1"
+  # shared_credentials_files = ["~/.aws/config"]
+}
+
+resource "aws_instance" "frontend" {
+  ami                     = data.aws_ami.centos8.image_id
+  instance_type           = var.instance_type
+  vpc_security_group_ids  = [var.vpc_security_group_ids]
+
+  tags = {
+    Name = "frontend-dev"
+  }
+}
+
+resource "aws_route53_record" "frontend" {
+  name    = "frontend-dev"
+  type    = "A"
+  ttl     = 30
+  zone_id = var.zone_id #Copy from console
+  records = [aws_instance.frontend.public_ip]
+}
+
