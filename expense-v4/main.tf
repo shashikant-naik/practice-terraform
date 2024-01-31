d91ee0e2f95c7312674a2de3cc497861ac62f347
SHASHIKANT NAIK <shashikant.naik@microfocus.com>
2024-02-01 00:01:03 +0530

    exp-v2

--- expense-v4/main.tf
+++ expense-v4/main.tf
@@ -6,20 +6,21 @@
 resource "aws_instance" "instances" {
   for_each                = var.components
   ami                     = data.aws_ami.centos8.image_id
-  instance_type           = var.instance_type
+  instance_type           = lookup(each.value,"instance_type","t2.micro")
   vpc_security_group_ids  = [var.vpc_security_group_ids]
 
   tags = {
-    Name = "${lookup(each.value, "name","none")}-dev"
+    Name = lookup(each.value, "name",null)-dev"
   }
 }
 
 resource "aws_route53_record" "records" {
-  count   = length(var.components)
-  name    = "${element(var.components, count.index)}-dev"
-  type    = "A"
-  ttl     = 30
-  zone_id = var.zone_id #Copy from console
-  records = [element(aws_instance.instances.*.private_ip, count.index)]
+  for_each = var.components
+  name     = lookup(each.value, "name", null)
+  type     = "A"
+  ttl      = 30
+  zone_id  = var.zone_id #Copy from console
+  records  = [lookup(lookup(aws_instance.instances, each.key, null)),"private_ip", null)]
+  [element(aws_instance.instances.*.private_ip, count.index)]
 }
 
