df3f01d6c9370d47f8a37d100903dcbd0433386e
SHASHIKANT NAIK <shashikant.naik@microfocus.com>
2024-01-31 23:24:56 +0530

    exp-v2

---
+++ expense-v4/main.tf
@@ -0,0 +1,25 @@
+#provider "aws" {
+#  region = "us-east-1"
+#  # shared_credentials_files = ["~/.aws/config"]
+#}
+
+resource "aws_instance" "instances" {
+  for_each                = var.components
+  ami                     = data.aws_ami.centos8.image_id
+  instance_type           = var.instance_type
+  vpc_security_group_ids  = [var.vpc_security_group_ids]
+
+  tags = {
+    Name = "${lookup(each.value, "name","none")}-dev"
+  }
+}
+
+resource "aws_route53_record" "records" {
+  count   = length(var.components)
+  name    = "${element(var.components, count.index)}-dev"
+  type    = "A"
+  ttl     = 30
+  zone_id = var.zone_id #Copy from console
+  records = [element(aws_instance.instances.*.private_ip, count.index)]
+}
+
